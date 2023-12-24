71a05b27abeaf304c8d352859ce41252ae17b6d2
nwin <134930309+nandarwin19@users.noreply.github.com>
2023-12-24 12:56:43 +0630

    Add files via upload

---
+++ src/components/Sldebar/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import './Sidebar.css'
+import Logo from '../../imgs/logo.png'
+import {UilEstate, UilSignOutAlt} from "@iconscout/react-unicons";
+import { SidebarData } from '../../Data/Data';
+
+const Sidebar = () => {
+
+    const [selected, setSelected] = useState(0);
+  return (
+    <div className="Sidebar">
+        {/* logo */}
+        <div className="logo">
+            <img src={Logo} alt="" />
+            <span>
+                Sh<span>o</span>ps
+            </span>
+        </div>
+
+        {/* menu  */}
+        <div className="menu">
+            {SidebarData.map((item, index) => {
+                return (
+                    <div className={selected===index?'menuItem active': 'menuItem'}
+                    key={index}
+                    onClick={() => setSelected(index)}
+                    >
+                        <div>
+                            <item.icon/>
+                        </div>
+                        <span>
+                            {item.heading}
+                        </span>
+                    </div>
+                )
+            })}
+
+            <div className="menuItem">
+                <UilSignOutAlt/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
